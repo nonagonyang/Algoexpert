dc1565a71c6724b163fb5e93700667c9e0003757
YANG LIU <>
2022-10-25 10:20:01 +0100

    work on valid BST

--- BST.py
+++ BST.py
@@ -38,3 +38,37 @@ def helper(tree, target, closest_candidate):
         else:
             break
     return closest_candidate
+
+
+#Check if a tree is a valid BST
+
+#from top to bottom check if current node is None
+#check if its left tree is valid BST and check if its right tree is BST
+# This is an input class. Do not edit.
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+#divide and conquer
+#validate any node's left and right trees
+#each node has a minimal value and maximum value
+#left node < direct parent
+#updating min and max when traversing the tree
+#gradually narrow down the min and max of node.
+#T: O(N) need to traverse every node in the tree
+#S: O(log(N)) using space on call stack O(D), D is the depth of the tree.
+
+def validateBst(tree):
+    return helper(tree, float("-inf"),float("inf"))
+def helper(tree, min, max):
+    if tree is None:
+        return True
+    if tree.value < min or tree.value >= max:
+        return False
+    #check subtrees validity
+    leftIsValid=helper(tree.left, min, tree.value)
+    rightIsValid=helper(tree.right,tree.value,max)
+    return leftIsValid and rightIsValid
